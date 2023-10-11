c3f900f7c92aba83fcb6222e39b13f303a2b9e49
(DevLangaliya) <(langaliya.dev@gmail.com)>
2023-10-10 20:29:21 -0500

    first real `git commit` with `git commit -m "message"`

--- src/App.js
+++ src/App.js
@@ -5,19 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Personal Webpage</h1>
       </header>
+      <main className="App-main">
+        <p>This is the main content of my personal webpage.</p>
+      </main>
+      <footer className="App-footer">
+        <p>Copyright © 2023 Bard AI</p>
+      </footer>
     </div>
   );
 }
