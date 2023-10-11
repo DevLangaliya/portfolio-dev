80dcb8e68730b9fe23ba51bcb5de558ddb736194
(DevLangaliya) <(langaliya.dev@gmail.com)>
2023-10-10 21:46:34 -0500

    images

--- src/Footer.js
+++ src/Footer.js
@@ -2,16 +2,18 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer class = "App-footer">
-      <p>Copyright &copy; 2023 My Name</p>
-      <ul>
-        <li>
-          <a href="https://www.github.com/DevLangaliya">Twitter</a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/dev-langaliya">LinkedIn</a>
-        </li>
-      </ul>
+    <footer className="App-footer">
+      <div style={{ textAlign: "center" }}>
+        <div>
+          <img src="./github.png" alt="Dev Langaliya" />
+        </div>
+        <a href="https://www.github.com/DevLangaliya" target="_blank" rel="noopener noreferrer">
+           GitHub
+        </a>
+        <a href="https://www.linkedin.com/in/dev-langaliya" target="_blank" rel="noopener noreferrer">
+          <img src="./linkedin.png" alt="Dev Langaliya" /> LinkedIn
+        </a>
+      </div>
     </footer>
   );
 }
