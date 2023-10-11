6c8a980bb681f8abf147ae63ef07f91b3b3f2b8b
(DevLangaliya) <(langaliya.dev@gmail.com)>
2023-10-11 01:23:55 -0500

    fixed footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,11 +5,10 @@ export default function Footer() {
     <footer className="App-footer">
       <div style={{ textAlign: "center" }}>
         <a href="https://www.github.com/DevLangaliya" target="_blank" rel="noopener noreferrer">
-          <img src="./github.png" alt="Dev Langaliya" width="32" /> GitHub
+          <img src="./github.png" alt="Dev Langaliya" width="32" />
         </a>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <a href="https://www.linkedin.com/in/dev-langaliya" target="_blank" rel="noopener noreferrer">
-          <img src="./linkedin.png" alt="Dev Langaliya" width="32" /> LinkedIn
+          <img src="./linkedin.png" alt="Dev Langaliya" width="32" />
         </a>
       </div>
     </footer>
