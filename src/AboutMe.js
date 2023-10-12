06e48e0a0ede1dbd00b7b291a3c3c847a7a76566
(DevLangaliya) <(langaliya.dev@gmail.com)>
2023-10-11 20:26:35 -0500

    fixed some formatting stuff

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -5,49 +5,51 @@ import Fade from 'react-reveal/Fade';
 export default function AboutMe() {
   return (
     <main className="App-aboutme">
-    <Fade>
+      <Fade>
         <h1>About Me</h1>
-    </Fade>
-    <div class="slides">
-        <Fade bottom>
-          <div class="carousel-item active">
-            <p>
-              I am a Mathematics and Econometrics student at the University of Illinois at Urbana-Champaign.
-            </p>
-          </div>
-        </Fade>
+      </Fade>
+      <div class="slides-container">
+        <div class="slides">
+          <Fade bottom>
+            <div class="carousel-item active">
+              <p>
+                I am a Mathematics and Econometrics student at the University of Illinois at Urbana-Champaign.
+              </p>
+            </div>
+          </Fade>
 
-        <Fade bottom right>
-          <div class="carousel-item ">
-            <p>
-              I am a software engineer with a passion for building web applications.
-            </p>
-          </div>
-        </Fade>
+          <Fade bottom right>
+            <div class="carousel-item ">
+              <p>
+                I am a software engineer with a passion for building web applications.
+              </p>
+            </div>
+          </Fade>
 
-        <Fade bottom right>
-          <div class="carousel-item ">
-            <p>
-              I am a software engineer with a passion for building web applications.
-            </p>
-          </div>
-        </Fade>
+          <Fade bottom right>
+            <div class="carousel-item ">
+              <p>
+                I am a software engineer with a passion for building web applications.
+              </p>
+            </div>
+          </Fade>
 
-        <Fade bottom right>
-          <div class="carousel-item ">
-            <p>
-              I am a software engineer with a passion for building web applications.
-            </p>
-          </div>
-        </Fade>
-        
-        <Fade bottom right>
-          <div class="carousel-item ">
-            <p>
-              I am a software engineer with a passion for building web applications.
-            </p>
-          </div>
-        </Fade>
+          <Fade bottom right>
+            <div class="carousel-item ">
+              <p>
+                I am a software engineer with a passion for building web applications.
+              </p>
+            </div>
+          </Fade>
+
+          <Fade bottom right>
+            <div class="carousel-item ">
+              <p>
+                I am a software engineer with a passion for building web applications.
+              </p>
+            </div>
+          </Fade>
+        </div>
       </div>
     </main>
   );
