c59171266807b64d438d4992efe14fe1db0cc0df
(DevLangaliya) <(langaliya.dev@gmail.com)>
2023-10-11 19:13:39 -0500

    MASSIV E CHANGES LETS GOOOOO

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import Header from "./Header.js";
-import Main from "./Main.js";
-import Card from "./Cards.js";
+import AboutMe from "./AboutMe.js";
+import Projects from "./Projects.js";
 import Footer from "./Footer.js";
 
 function App() {
 
   return (
-    <div id = "main-div">
-      <Header />
-      <Main />
-      <Card />
+    <div id="main-div">
+      <div id="main-first-connect">
+        <Header />     
+        <AboutMe />
+      </div>
+      <Projects />
       <Footer />
     </div>
   );
---
+++ src/Projects.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Projects.css';
+import { Fade } from 'react-reveal';
+
+export default function Projects() {
+    return (
+        <div className="App-projects-container">
+            <Fade right>
+                <div className="App-project">
+                    <h2>Project 1</h2>
+                    <p>This is the content for project 1.</p>
+                </div>
+            </Fade>
+            <Fade left>
+                <div className="App-project">
+                    <h2>Project 2</h2>
+                    <p>This is the content for project 2.</p>
+                </div>
+            </Fade>
+            <Fade right>
+                <div className="App-project">
+                    <h2>Project 3</h2>
+                    <p>This is the content for project 3.</p>
+                </div>
+            </Fade>
+            <Fade left>
+                <div className="App-project">
+                    <h2>Project 4</h2>
+                    <p>This is the content for project 4.</p>
+                </div>
+            </Fade>
+        </div>
+    );
+}
