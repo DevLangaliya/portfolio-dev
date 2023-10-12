ae0dc0a94ddf4bb2e9482f711a91e6718e428d1d
(DevLangaliya) <(langaliya.dev@gmail.com)>
2023-10-11 20:03:58 -0500

    hjkl

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -5,33 +5,32 @@ import Fade from 'react-reveal/Fade';
 export default function AboutMe() {
   return (
     <main className="App-aboutme">
-     <Fade>
+    <Fade>
         <h1>About Me</h1>
-      </Fade>
-      <div class="slides">
-          <Fade bottom>
-            <div class="carousel-item active">
-              <p>
-                I am a software engineer with a passion for building web applications.
-              </p>
-            </div>
-          </Fade>
-          <Fade bottom right>
-            <div class="carousel-item ">
-              <p>
-                I am a software engineer with a passion for building web applications.
-              </p>
-            </div>
-          </Fade>
-          <Fade bottom right>
-            <div class="carousel-item ">
-              <p>
-                I am a software engineer with a passion for building web applications.
-              </p>
-            </div>
-          </Fade>
+    </Fade>
+    <div class="slides">
+        <Fade bottom>
+          <div class="carousel-item active">
+            <p>
+              I am a software engineer with a passion for building web applications.
+            </p>
           </div>
-      
+        </Fade>
+        <Fade bottom right>
+          <div class="carousel-item ">
+            <p>
+              I am a software engineer with a passion for building web applications.
+            </p>
+          </div>
+        </Fade>
+        <Fade bottom right>
+          <div class="carousel-item ">
+            <p>
+              I am a software engineer with a passion for building web applications.
+            </p>
+          </div>
+        </Fade>
+      </div>
     </main>
   );
 }
