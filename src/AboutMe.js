09993c8bfc0e15895084ca6787f7474f6784de6b
(DevLangaliya) <(langaliya.dev@gmail.com)>
2023-10-11 20:26:16 -0500

    scroll bar no longer appears on the about me section

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -12,10 +12,11 @@ export default function AboutMe() {
         <Fade bottom>
           <div class="carousel-item active">
             <p>
-              I am a software engineer with a passion for building web applications.
+              I am a Mathematics and Econometrics student at the University of Illinois at Urbana-Champaign.
             </p>
           </div>
         </Fade>
+
         <Fade bottom right>
           <div class="carousel-item ">
             <p>
@@ -23,6 +24,23 @@ export default function AboutMe() {
             </p>
           </div>
         </Fade>
+
+        <Fade bottom right>
+          <div class="carousel-item ">
+            <p>
+              I am a software engineer with a passion for building web applications.
+            </p>
+          </div>
+        </Fade>
+
+        <Fade bottom right>
+          <div class="carousel-item ">
+            <p>
+              I am a software engineer with a passion for building web applications.
+            </p>
+          </div>
+        </Fade>
+        
         <Fade bottom right>
           <div class="carousel-item ">
             <p>
