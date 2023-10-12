5ba0ffe2a91f89eeafcd8029e255fd338c82f388
(DevLangaliya) <(langaliya.dev@gmail.com)>
2023-10-12 00:08:21 -0500

    added projects and more better things

--- src/Projects.js
+++ src/Projects.js
@@ -7,8 +7,10 @@ export default function Projects() {
         <div className="App-projects-container">
             <Fade right>
                 <div className="App-project">
-                    <h2>Project 1</h2>
-                    <p>This is the content for project 1.</p>
+                    <h2>Java Dynasty Simulator</h2>
+                    <li>American Football simulation game where the user manages an American Football team through a 17 week season.</li>
+                    <li>Watch games unfold during real time with a play-by-play of each team’s possessions in a new window.</li>
+                    <li>Used statistical methods to show the cumulative stats for players across all games in a graphical window view.</li>
                 </div>
             </Fade>
 
