ee9b3b40a04943ee15d91620d1e7e0b2922332eb
(DevLangaliya) <(langaliya.dev@gmail.com)>
2023-10-18 16:03:59 -0500

    fixed local machine errors

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./Header.js";
 import AboutMe from "./AboutMe.js";
 import Projects from "./Projects.js";
 import Footer from "./Footer.js";
+import React from 'react';
 
 function App() {
 
